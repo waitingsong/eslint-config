6243f63f4e5aec5fce7ca89d3c4e3fe90a139a58
waiting <1661926154@qq.com>
2024-03-21 18:35:08 +0800

    feat: up

---
+++ src/recommended-for-test.mjs
@@ -0,0 +1,51 @@
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+import base from './base.mjs'
+import importPluginRules from './plugin/import.mjs'
+import unicornPluginRules from './plugin/unicorn.mjs'
+import { stylesRules, lintRules } from './recommended-rules.mjs'
+
+
+// https://eslint.style/packages/default
+const styles = [
+  stylistic.configs['recommended-flat'],
+  {
+    rules: {
+      ...stylesRules,
+    },
+  },
+]
+
+
+const testConfig = {
+  // files: ['packages/*/test/**', 'test/**'],
+  languageOptions: {
+    globals: {
+      ...globals.mocha,
+    },
+  },
+  rules: {
+    ...lintRules,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/prefer-ts-expect-error': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-console': 0,
+    'unicorn/filename-case': 0,
+  },
+}
+
+export default [
+  ...base,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...styles,
+  ...importPluginRules,
+  ...unicornPluginRules,
+  testConfig,
+]
+
+
