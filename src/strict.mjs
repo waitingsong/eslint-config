d11fb6d6207073209977d4f3eb5e13fdc69177e2
waiting <1661926154@qq.com>
2024-03-22 15:20:40 +0800

    feat(strict): enable strict rules from typescript-eslint
    - strict
    - strictTypeChecked

--- src/strict.mjs
+++ src/strict.mjs
@@ -1,9 +1,13 @@
+import tseslint from 'typescript-eslint'
+
 import recommended from './recommended.mjs'
 import { strictRules } from './strict-rules.mjs'
 
 
 export default [
   ...recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.strictTypeChecked,
   { rules: strictRules },
 ]
 
